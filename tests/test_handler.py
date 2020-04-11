e19e289cd85aa8f5de91d413a5a61f57ad6e8bd4
Greg Woodfill <greg@gregwoodfill.com>
2020-04-11 11:35:01 -0500

    See if gh build fails

--- tests/test_handler.py
+++ tests/test_handler.py
@@ -4,3 +4,7 @@ from edina_garbage.handler import lambda_handler
 def test_returns_200():
     response = lambda_handler(None, None)
     assert response == {'body': '"Hello from lambda!"', 'status': 200}
+
+
+def test_break():
+    assert False
